bf9eeaaa06ba0c8a98185a4fd3b2bb4bd0a3c816
Hariharan <hari07.at@gmail.com>
2021-06-23 20:36:25 +0530

    Completed top Navbar

--- src/app/shared_components/top_nav_bar/TopNavBar.tsx
+++ src/app/shared_components/top_nav_bar/TopNavBar.tsx
@@ -1,21 +1,87 @@
+import { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { auth } from '../../../firebase';
 import useAuth from '../../../hooks/useAuth';
+import './TopNavBar.scss';
 
 const logo = `${process.env.PUBLIC_URL}/assets/images/tdca_logo.jpg`;
 
-const TopNavBar = () => {
+const TopNav = () => {
     const authData = useAuth();
+    const [isMobileOpen, toggleMobileOpen] = useState(false);
 
     return (
-        <div className="navBar">
-            <div className="navBarContainer">
-                <div className="navLogo">
-                    <img src={logo} alt="Logo" />
+        <div className="nav">
+            <div className="nav__header">
+                <div>
+                    <img src={logo} alt="Logo" className="nav__header--logo" />
+                </div>
+                <div>
+                    <h1 className="nav__header--text">Trichy District Cricket Association</h1>
+                    <h1 className="nav__header--textAbbr">TDCA</h1>
                 </div>
             </div>
+            <div> 
+            <button type="button" className="hamburger" 
+            onClick={() => toggleMobileOpen(!isMobileOpen)}>
+                    {isMobileOpen ? <FaTimes /> : <FaBars />}
+            </button>
+            </div>
+            <div  className={isMobileOpen ? 'nav__responsive' : 'nav__body'}>
+               
+                    <div className = "item">
+                        <Link to="/" className="nav__link">
+                            Home
+                        </Link>
+                    </div>
+                    <div className = "item">
+                        <Link to="/" className="nav__link">
+                            Matches
+                        </Link>
+                    </div>
+                    <div className = "item">
+                        <Link to="/" className="nav__link">
+                            Teams
+                        </Link>
+                    </div>
+                    <div className = "item">
+                        <Link to="/" className="nav__link">
+                            Staffs
+                        </Link>
+                    </div>
+                    <div className = "item">
+                        <Link to="/" className="nav__link">
+                            Contact
+                        </Link>
+                    </div>
+                
+          
+                    {authData === undefined ? (
+                        <div>  
+                        <Link to="/" className="nav__btn"  onClick={async()=>{
+                           await auth.signInAnonymously();
+                        }}>
+                            Log In
+                        </Link>
+                        </div>
+         
+                    ) : (
+                        <div>
+                        <Link to="/" className="nav__btn" onClick={async()=>{
+                            await auth.signOut();}}>
+                            Log Out
+                        </Link>
+                        </div>
+            
+                    )}
+           
+             
+               
+            </div>
+          
         </div>
     );
 };
 
-export default TopNavBar;
+export default TopNav;
