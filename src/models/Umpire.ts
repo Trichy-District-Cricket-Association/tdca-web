aa4a51099ef15a37d387fe788a9e96ebf6b01b3a
Karthikeyan S <karthikeyanssvk@gmail.com>
2021-06-25 19:38:12 +0530

    add: model for scorer, umpire

--- src/models/Umpire.ts
+++ src/models/Umpire.ts
@@ -1,7 +1,97 @@
-export default class Umpire {
-    id: number;
+import { DivisionMatches } from './model_types/DivisionMatches';
+import { TypeMatches } from './model_types/TypeMatches';
 
-    constructor(id: number) {
-        this.id = id;
+export default class Umpire {
+    /** Document id of the Umpire document. */
+    docId?: string;
+
+    umpireId: string;
+
+    umpireName: string;
+
+    avatarUrl?: string;
+
+    emailId: string;
+
+    divisionMatches: DivisionMatches;
+
+    typeMatches: TypeMatches;
+
+    totalMatches: number;
+
+    totalDue: number;
+
+    totalPaid: number;
+
+    balanceAmount: number;
+
+    panel: string;
+
+    dateOfBirth: Date;
+
+    primaryContact: string;
+
+    secondaryContact: string;
+
+    payPhoneNumber: string;
+
+    bankAccount: string;
+
+    bankName: string;
+
+    bankBranch: string;
+
+    bankIFSC: string;
+
+    aadharNumber: string;
+
+    permanentAddress: string;
+
+    constructor({
+        docId,
+        umpireId,
+        umpireName,
+        avatarUrl,
+        emailId,
+        divisionMatches,
+        typeMatches,
+        totalMatches,
+        totalDue,
+        totalPaid,
+        balanceAmount,
+        panel,
+        dateOfBirth,
+        primaryContact,
+        secondaryContact,
+        payPhoneNumber,
+        bankAccount,
+        bankName,
+        bankBranch,
+        bankIFSC,
+        aadharNumber,
+        permanentAddress,
+    }: Umpire) {
+        if (docId) this.docId = docId;
+        this.umpireId = umpireId;
+        this.umpireName = umpireName;
+        if (avatarUrl) this.avatarUrl = avatarUrl;
+        this.emailId = emailId;
+        this.divisionMatches = divisionMatches;
+        this.typeMatches = typeMatches;
+        this.totalMatches = totalMatches;
+        this.totalDue = totalDue;
+        this.totalPaid = totalPaid;
+        this.balanceAmount = balanceAmount;
+        this.panel = panel;
+        this.dateOfBirth = dateOfBirth;
+        this.primaryContact = primaryContact;
+        this.secondaryContact = secondaryContact;
+        this.payPhoneNumber = payPhoneNumber;
+        this.bankAccount = bankAccount;
+        this.bankName = bankName;
+        this.bankBranch = bankBranch;
+        this.bankIFSC = bankIFSC;
+        this.aadharNumber = aadharNumber;
+        this.permanentAddress = permanentAddress;
     }
 }
---
+++ src/models/model_types/DivisionMatches.ts
@@ -0,0 +1,7 @@
+export type DivisionMatches = {
+    one: number;
+    two: number;
+    three: number;
+    four: number;
+    five: number;
+};
