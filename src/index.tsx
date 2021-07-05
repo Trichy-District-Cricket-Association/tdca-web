cf1d3fbd67778cb2edfee6641601c1539274a70f
Karthikeyan S <karthikeyanssvk@gmail.com>
2021-07-05 22:36:59 +0530

    fix: add basename for browserrouter

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,12 @@ import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
